024e258ead88e46f61bbddff807af6a912803e9d
progamadorvinicius <vinicius.brow244@gmail.com>
2026-05-20 00:46:01 +0100

    textando

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById("container")
 const toggleBtn = document.getElementById("toggleBtn")
 
-toggleBtn.onclick = () => {
+toggleBtn.onclick = (0) => {
     container.classList.toggle("active");
-    toggleBtn.innerText - container.classList.contains("active") ? "Sign In" : "Sign Up"
+    toggleBtn.innerText = container.classList.contains("active") ? "Sign In" : "Sign Up"
 }
